79157d440b89ddb8dc556b56d7a012259f528022
Corina717 <corina.simionescu12@gmail.com>
2024-11-03 22:21:44 +0200

    Adaugare seminar 5

---
+++ Seminare/app/seminar_5/src/main/java/com/example/seminar_5/Caine.java
@@ -0,0 +1,120 @@
+package com.example.seminar_5;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class Caine implements Parcelable {
+    private String nume;
+    private boolean esteAgresiv;
+    private String rasa;
+    private String dimensiune;
+    private float nivelDeDragutenie;
+    private boolean esteJucaus;
+
+    public Caine(String nume, boolean esteAgresiv, String rasa, String dimensiune, float nivelDeDragutenie, boolean esteJucaus) {
+        this.nume = nume;
+        this.esteAgresiv = esteAgresiv;
+        this.rasa = rasa;
+        this.dimensiune = dimensiune;
+        this.nivelDeDragutenie = nivelDeDragutenie;
+        this.esteJucaus = esteJucaus;
+    }
+
+    protected Caine(Parcel in) {
+        nume = in.readString();
+        esteAgresiv = in.readByte() != 0;
+        rasa = in.readString();
+        dimensiune = in.readString();
+        nivelDeDragutenie = in.readFloat();
+        esteJucaus = in.readByte() != 0;
+    }
+
+    public static final Creator<Caine> CREATOR = new Creator<Caine>() {
+        @Override
+        public Caine createFromParcel(Parcel in) {
+            return new Caine(in);
+        }
+
+        @Override
+        public Caine[] newArray(int size) {
+            return new Caine[size];
+        }
+    };
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public boolean isEsteAgresiv() {
+        return esteAgresiv;
+    }
+
+    public void setEsteAgresiv(boolean esteAgresiv) {
+        this.esteAgresiv = esteAgresiv;
+    }
+
+    public String getRasa() {
+        return rasa;
+    }
+
+    public void setRasa(String rasa) {
+        this.rasa = rasa;
+    }
+
+    public String getDimensiune() {
+        return dimensiune;
+    }
+
+    public void setDimensiune(String dimensiune) {
+        this.dimensiune = dimensiune;
+    }
+
+    public float getNivelDeDragutenie() {
+        return nivelDeDragutenie;
+    }
+
+    public void setNivelDeDragutenie(float nivelDeDragutenie) {
+        this.nivelDeDragutenie = nivelDeDragutenie;
+    }
+
+    public boolean isEsteJucaus() {
+        return esteJucaus;
+    }
+
+    public void setEsteJucaus(boolean esteJucaus) {
+        this.esteJucaus = esteJucaus;
+    }
+
+    @Override
+    public String toString() {
+        return "Caine{" +
+                "nume='" + nume + '\'' +
+                ", esteAgresiv=" + esteAgresiv +
+                ", rasa='" + rasa + '\'' +
+                ", dimensiune='" + dimensiune + '\'' +
+                ", nivelDeDragutenie=" + nivelDeDragutenie +
+                ", esteJucaus=" + esteJucaus +
+                '}';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeString(nume);
+        dest.writeByte((byte) (esteAgresiv ? 1 : 0));
+        dest.writeString(rasa);
+        dest.writeString(dimensiune);
+        dest.writeFloat(nivelDeDragutenie);
+        dest.writeByte((byte) (esteJucaus ? 1 : 0));
+    }
+}
---
+++ Seminare/app/seminar_5/src/main/java/com/example/seminar_5/ListaCainiActivity.java
@@ -0,0 +1,56 @@
+package com.example.seminar_5;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.List;
+
+public class ListaCainiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_lista_caini);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent intent = getIntent();
+        List<Caine> caini = intent.getParcelableArrayListExtra("caini");
+
+        ListView listViewCaini = findViewById(R.id.listViewCaini);
+        ArrayAdapter<Caine> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, caini);
+        listViewCaini.setAdapter(adapter);
+
+        listViewCaini.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), caini.get(position).toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        listViewCaini.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                caini.remove(position);
+                adapter.notifyDataSetChanged();
+
+                return false;
+            }
+        });
+    }
+}
